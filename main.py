4a0a7fcf368d803bdbe227289c4346991dcee7c2
Harold Pulcher <pulcher@pulcher.biz>
2023-05-07 14:17:09 -0500

    got the quick trip in very close.... perfect run on one attempt.

--- main.py
+++ main.py
@@ -3,35 +3,38 @@ def doSquare():
         yellow yellow yellow yellow yellow yellow yellow yellow yellow yellow
     """)
     control.wait_micros(4000000)
+    crickit.tank(0, 100)
+    control.wait_micros(5000000)
+    crickit.tank(0, 0)
+    light.show_ring("""
+        red red red red red red red red red red
+    """)
 def doLine():
     light.show_ring("""
         purple purple purple purple purple purple purple purple purple purple
     """)
-    control.wait_micros(4000000)
-    crickit.tank(45, 50)
-    control.wait_micros(15000000)
+    control.wait_micros(100000)
+    crickit.tank(93, 100)
+    control.wait_micros(5000000)
     crickit.tank(0, 0)
+    control.wait_micros(500000)
+    crickit.tank(-92, -100)
+    control.wait_micros(5000000)
+    crickit.tank(0, 0)
+    light.show_ring("""
+        red red red red red red red red red red
+    """)
 
 def on_button_a_click():
-    global DoRoutine
-    DoRoutine = 1
+    doLine()
 input.button_a.on_event(ButtonEvent.CLICK, on_button_a_click)
 
-DoRoutine = 0
+def on_button_b_click():
+    doSquare()
+input.button_b.on_event(ButtonEvent.CLICK, on_button_b_click)
+
 DoRoutine = 0
 
 def on_forever():
-    global DoRoutine
-    if DoRoutine:
-        light.show_ring("""
-            green green green green green green green green green green
-        """)
-        control.wait_micros(4000000)
-        if input.switch_right():
-            doSquare()
-        else:
-            doLine()
-        DoRoutine = 0
-    else:
-        light.show_ring("red red red red red red red red red red")
+    pass
 forever(on_forever)
--- main.ts
+++ main.ts
@@ -3,40 +3,39 @@ function doSquare () {
     `yellow yellow yellow yellow yellow yellow yellow yellow yellow yellow`
     )
     control.waitMicros(4000000)
+    crickit.tank(0, 100)
+    control.waitMicros(5000000)
+    crickit.tank(0, 0)
+    light.showRing(
+    `red red red red red red red red red red`
+    )
 }
 function doLine () {
     light.showRing(
     `purple purple purple purple purple purple purple purple purple purple`
     )
-    control.waitMicros(4000000)
-    crickit.tank(45, 50)
-    control.waitMicros(12000000)
+    control.waitMicros(100000)
+    crickit.tank(100, 100)
+    control.waitMicros(5000000)
     crickit.tank(0, 0)
-    control.waitMicros(4000000)
-    crickit.tank(-45, -50)
-    control.waitMicros(12000000)
+    control.waitMicros(1000000)
+    light.showRing(
+    `orange orange orange orange orange orange orange orange orange orange`
+    )
+    crickit.tank(-100, -100)
+    control.waitMicros(5000000)
     crickit.tank(0, 0)
+    light.showRing(
+    `red red red red red red red red red red`
+    )
 }
 input.buttonA.onEvent(ButtonEvent.Click, function () {
-    DoRoutine = 1
+    doLine()
+})
+input.buttonB.onEvent(ButtonEvent.Click, function () {
+    doSquare()
 })
 let DoRoutine = 0
-DoRoutine = 0
 forever(function () {
-    if (DoRoutine) {
-        light.showRing(
-        `green green green green green green green green green green`
-        )
-        control.waitMicros(4000000)
-        if (input.switchRight()) {
-            doSquare()
-        } else {
-            doLine()
-        }
-        DoRoutine = 0
-    } else {
-        light.showRing(
-        "red red red red red red red red red red"
-        )
-    }
+	
 })
